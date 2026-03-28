6a4c0bcb7f361f1847bf202d60eb819ac883e35f
MdRohit786 <middyarohit0@gmail.com>
2026-03-29 00:19:13 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json()); //to parse req.body
+app.use(express.urlencoded({extended:true}))
+app.use("/api/auth",authRoutes);
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
+});
+
