cfa7d34726bb70072ba0c51313e35d51b8b5365e
MdRohit786 <middyarohit0@gmail.com>
2026-04-15 21:00:25 +0530

    define getme and protect router

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -103,9 +103,12 @@ export const logout = async (req, res) => {
 export const getMe = async(req,res)=>{
   
   try {
-    const user = await User.findById(req.user._id);
+    const user = await User.findById(req.user._id).select("-password");
+    res.status(200).json(user);
 
   } catch (error) {
+    console.log("Error in getme controller", error.message);
+    res.status(500).json({error:"internal server error"});
     
   }
 }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { getMe, login, logout, signup } from "../controllers/auth.controller.js";
-
+import {protectRoute} from '../middleware/protectRoute.js'
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/getme",protectRoute,getMe);
+router.get("/getme",protectRoute,getMe);
 
 export default router;
 
