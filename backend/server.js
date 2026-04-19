1dd0fa1b012d471f9d3ce2fa3ce6e9bdbe7bb9e8
MdRohit786 <middyarohit0@gmail.com>
2026-04-19 23:19:58 +0530

    working on user follow/unfollow functionality

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -14,6 +15,7 @@ app.use(express.urlencoded({extended:true}))
 
 app.use(cookieParser());
 app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
 
 
 app.listen(PORT,()=>{
