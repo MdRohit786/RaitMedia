9d0cd6b80d9c397d9258ff9b1c687f762828aa38
MdRohit786 <middyarohit0@gmail.com>
2026-06-30 21:40:45 +0530

    adding like post backend functunality

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -102,7 +102,8 @@ export const likeUnlikePost = async(req,res)=>{
             //unlike post
             await Post.updateOne({_id:postId},{$pull:{likes:userId}});
             await User.updateOne({_id: userId},{$pull:{likedPosts:postId}});
-            res.status(200).json({message:"Post unliked successfully"})
+            const updatedlikes  = post.likes.filter((id)=> id.toString() !== userId.toString());
+            res.status(200).json(updatedlikes);
         }else{
             //like post
             post.likes.push(userId);
@@ -115,8 +116,8 @@ export const likeUnlikePost = async(req,res)=>{
                 type: "like"
             });
             await notification.save();
-
-            res.status(200).json({message:"Post liked successfully"});
+            const updatedlikes = post.likes;
+            res.status(200).json(updatedlikes);
         }
     } catch (error) {
         console.log("Error in LikeUnlikePost Controller: ",error);
